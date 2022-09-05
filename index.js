4fbba97e05385233271a9ec012fed4b40cf6ccec
Samandar [flexit] <uchqunovsamandar31@gmail.com>
2022-09-06 04:37:04 +0500

    initial graphql server

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const graphql = require('graphql')
+const { graphqlHTTP } = require('express-graphql')
+
+const data = require('./mockdata.json')
+
+const app = express()
+const userType = new graphql.GraphQLObjectType({
+    name: "user",
+    fields: {
+        id: { type: graphql.GraphQLInt },
+        firstName: { type: graphql.GraphQLString },
+        lastName: { type: graphql.GraphQLString },
+        email: { type: graphql.GraphQLString },
+        password: { type: graphql.GraphQLString },
+    }
+})
+const rootQuery = new graphql.GraphQLObjectType({
+    name: "rootQueryType",
+    fields: {
+        getAllUser: {
+            type: new graphql.GraphQLList(userType),
+            args: { id: { type: graphql.GraphQLInt } },
+            resolve(parent, args) {
+                return data
+            }
+        }
+    }
+})
+const mutation = new graphql.GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser: {
+            type: new graphql.GraphQLList(userType),
+            args: {
+                firstName: { type: graphql.GraphQLString },
+                lastName: { type: graphql.GraphQLString },
+                email: { type: graphql.GraphQLString },
+                password: { type: graphql.GraphQLString },
+            },
+            resolve(parent, args) {
+                data.push({ id: data.length + 1, ...args })
+                return args
+            }
+        }
+    }
+})
+const schema = new graphql.GraphQLSchema({ query: rootQuery, mutation: mutation })
+app.use('', graphqlHTTP({
+    schema,
+    graphiql: true,
+
+}))
+app.listen(3000, () => {
+    console.log(`${3000} - portni eshityapman`)
+})
